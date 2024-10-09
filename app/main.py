7c2b06b959f3290d8f6ad5d7682744bf114de85d
hureramujeeb60 <hureram8@gmail.com>
2024-10-09 22:23:59 +0500

    added cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 import os
 # from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import dua, user, session, history
 from app.db import init_db, engine, metadata
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    )
 
 app.include_router(dua.router)
 app.include_router(user.router)
